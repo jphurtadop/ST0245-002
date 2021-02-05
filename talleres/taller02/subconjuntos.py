708d738ad9696f1a44ffbcd698b864403a26f3ed
Juan Pablo Hurtado Palacio <37987884+jphurtadop@users.noreply.github.com>
2021-02-04 20:54:29 -0500

    Add files via upload

---
+++ talleres/taller02/subconjuntos.py
@@ -0,0 +1,12 @@
+def subconjuntos(s):
+    subconjuntosBase("", s)
+
+def subconjuntosBase(base, t):
+    if len(t) == 0:
+        print(base)
+    else:
+        x = t[1:]
+        subconjuntosBase(base+t[x],t[0:x])
+        subconjuntosBase(base,t[x+1:] )
+
+print(subconjuntos("Santi"))
